f55550b001399e2db10f4b626874d5bc19bdbf3f
SaurabhGodse <godsesaurabhgsm@gmail.com>
2020-05-20 12:21:25 +0530

    Assignment 2 completed and result files added

--- Course2/HonorsAssignments/ImageInversion.java
+++ Course2/HonorsAssignments/ImageInversion.java
@@ -1,4 +1,5 @@
-
+import edu.duke.*;
+import java.io.*;
 /**
  * Write a description of ImageInversion here.
  * 
@@ -6,5 +7,29 @@
  * @version (a version number or a date)
  */
 public class ImageInversion {
+    public ImageResource makeInvert(ImageResource inImage){
+        ImageResource outImage = new ImageResource(inImage.getWidth(), inImage.getHeight());
+        for(Pixel pixel : outImage.pixels()){
+            Pixel inPixel = inImage.getPixel(pixel.getX(), pixel.getY());
+            pixel.setRed(255 - inPixel.getRed());
+            pixel.setGreen(255 - inPixel.getGreen());
+            pixel.setBlue(255 - inPixel.getBlue());
+        }
+        return outImage;
+    }
+    public void selectAndConvert(){
+        DirectoryResource dr = new DirectoryResource();
+        for(File f : dr.selectedFiles()){
+            ImageResource inImage = new ImageResource(f);
+            ImageResource outImage = makeInvert(inImage);
+            String name = inImage.getFileName();
+            String newName = "Images/invert-" + name;
+            outImage.setFileName(newName);
+            outImage.draw();
+            outImage.save();
+            
+        }
+    }
+
 
 }
