6b0276057019a4cb1083d938372842a019900af4
SaurabhGodse <godsesaurabhgsm@gmail.com>
2020-06-09 15:10:54 +0530

    project random text generation completed

---
+++ Course4/RandomTextStarterProgram/Tester.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import edu.duke.*;
+/**
+ * Write a description of Tester here.
+ * 
+ * @author Saurabh Godse
+ * @version (a version number or a date)
+ */
+public class Tester {
+    public void testGetFollows(){
+        String st = "this is a test yes this is a test.";
+        MarkovOne markov = new MarkovOne();
+        markov.setTraining(st);
+        System.out.println(markov.getFollows("."));
+        
+    }
+    public void testGetFollowsWithFile(){
+        FileResource fr = new FileResource();
+        String st = fr.asString();
+        st = st.replace('\n', ' ');
+        
+        MarkovOne markov = new MarkovOne();
+        markov.setTraining(st);
+        System.out.println(markov.getFollows("th").size());
+        
+    }
+
+}
